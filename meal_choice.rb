3d19c4da1fe4fccd35300a63b772a20e3ce1b0d1
demian sims <demiansims@gmail.com>
2018-10-11 15:06:40 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,6 +1,7 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
-def meal_choice()
+def meal_choice(meal_choice)
+  puts meal_choice
   puts "meat"
-  return meal_choice()
+  
 end
